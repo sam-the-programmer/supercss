6ad0cad66fd43d8f24e9e8d51c3bdff0d2dc7fad
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-11-14 12:22:15 +0000

    Fix CSS location errors

--- Scripts/text.go
+++ Scripts/text.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+const NEWLINE = ""
+
 func float_to_str(f float64) string {
 	return strings.ReplaceAll(fmt.Sprintf("%f", f/10), ".", "-")
 }
@@ -18,9 +20,11 @@ func gen_bgs(output string) string {
 		"purple", "pink", "gray",
 		"black", "brown",
 	} {
-		output += "\n.t-" + string(v) + "{color:" + string(v) + ";}.t-" + string(v) + " * {color: " + string(v) + ";}"
+		output += NEWLINE + ".t-" + string(v) + "{color:" + string(v) + ";}.t-" + string(v) + " * {color: " + string(v) + ";}"
+		output += NEWLINE + ".t-" + string(v) + "{color:" + string(v) + ";}.t-" + string(v) + " * {color: " + string(v) + ";}"
 		for i := 1; i < 4; i++ {
-			output += "\n.t-" + string(v) + strconv.Itoa(i) + "{color: var(--" + string(v) + strconv.Itoa(i) + ");}.t-" + string(v) + strconv.Itoa(i) + " * {color: var(--" + string(v) + strconv.Itoa(i) + ");}"
+			output += NEWLINE + ".t-" + string(v) + strconv.Itoa(i) + "{color: var(--" + string(v) + strconv.Itoa(i) + ");}.t-" + string(v) + strconv.Itoa(i) + " * {color: var(--" + string(v) + strconv.Itoa(i) + ");}"
+			output += NEWLINE + ".t-" + string(v) + strconv.Itoa(i) + "{text-shadow: var(--" + string(v) + strconv.Itoa(i) + ");}.t-" + string(v) + strconv.Itoa(i) + " * {color: var(--" + string(v) + strconv.Itoa(i) + ");}"
 		}
 	}
 
