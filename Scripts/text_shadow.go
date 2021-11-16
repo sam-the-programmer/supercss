83bf8ec6a752c63136f526f07c5c83b8122980b2
Password-Classified <85027459+Password-Classified@users.noreply.github.com>
2021-11-16 19:45:11 +0000

    add "dropped text"

--- Scripts/text_shadow.go
+++ Scripts/text_shadow.go
@@ -7,20 +7,24 @@ import (
 	"strings"
 )
 
+const newline = ""
+
 func floatToStr(f float64) string {
 	return strings.ReplaceAll(fmt.Sprintf("%f", f/10), ".", "-")
 }
 
-func genBgs(output string) string {
+func genGlows(output string) string {
 	for _, v := range []string{
 		"red", "orange", "yellow",
 		"green", "lightblue", "blue",
 		"purple", "pink", "gray",
 		"black", "brown",
 	} {
-		output += "\n.glow-" + string(v) + "{text-shadow: 0 0 .5ch" + string(v) + ";}"
+		output += newline + ".tglow-" + string(v) + "{text-shadow: 0 0 .5ch" + string(v) + ";}"
+		output += newline + ".tdrop-" + string(v) + "{text-shadow: .2ch .2ch 0" + string(v) + ";}"
 		for i := 1; i < 4; i++ {
-			output += "\n.glow-" + string(v) + strconv.Itoa(i) + "{text-shadow: 0 0 .5ch" + string(v) + ";}"
+			output += newline + ".tglow-" + string(v) + strconv.Itoa(i) + "{text-shadow: 0 0 .5ch" + string(v) + ";}"
+			output += newline + ".tglow-" + string(v) + strconv.Itoa(i) + "{text-shadow: .2ch .2ch 0" + string(v) + ";}"
 		}
 	}
 
@@ -38,9 +42,10 @@ func WriteFile(output string, filename string) {
 
 func main() {
 	output := `@import url('./base.css');
-.bg-white{background-color:white;}`
+.tglow-white{text-shadow: 0 0 .5ch:white;}
+.tdrop-whtie{text-shadow: .2ch .2ch 0:white;}`
 
-	output = genBgs(output)
+	output = genGlows(output)
 
 	WriteFile(output, "./CSS/glow-text.css")
 }
